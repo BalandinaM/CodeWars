63e558ef2ccef9bc5852f697afd127b07e3cadc7
BalandinaM <marina.balandina.89@mail.ru>
2026-02-24 11:28:34 +0500

    February 24

---
+++ fakeBin.js
@@ -0,0 +1,13 @@
+function fakeBin(x){
+    let newString = ""
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] < 5) {
+            newString += 0;
+        } else {
+            newString += 1;
+        }
+    }
+    return newString
+}
+
+console.log(fakeBin('45385593107843568'));
---
+++ lovefunc.js
@@ -0,0 +1,6 @@
+function lovefunc(flower1, flower2){
+  let sum = flower1+flower2
+  return sum%2 != 0 ? true : false
+}
+
+console.log(lovefunc(1,1));
