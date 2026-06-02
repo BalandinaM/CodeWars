dcf11a7bdee5fd9f131848f30acda9065e3f7584
BalandinaM <marina.balandina.89@mail.ru>
2026-06-02 23:31:56 +0500

    June 2_2

---
+++ june_2.js
@@ -0,0 +1,10 @@
+function arrayMash(array1, array2) {
+  let newArr = []
+  for (let i = 0; i < array1.length; i++) {
+    newArr.push(array1[i])
+    newArr.push(array2[i])
+  }
+  return newArr
+}
+
+console.log(arrayMash([1, 2, 3], ['a', 'b', 'c']))
